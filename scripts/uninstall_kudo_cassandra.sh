5f001eb6bd0da9948dc226059e9032bbdcbb6ea1
Murilo Pereira <murilo@murilopereira.com>
2019-10-04 21:45:53 +0200

    Make uninstall script take arguments instead of env vars.

--- scripts/uninstall_kudo_cassandra.sh
+++ scripts/uninstall_kudo_cassandra.sh
@@ -1,30 +1,63 @@
 #!/usr/bin/env bash
 
-readonly KUDO_CASSANDRA_OPERATOR_NAME="${KUDO_CASSANDRA_OPERATOR_NAME:-cassandra}"
-readonly KUDO_CASSANDRA_VERSION="${KUDO_CASSANDRA_VERSION:-0.1.0}"
-readonly KUDO_CASSANDRA_INSTANCE_NAME="${KUDO_CASSANDRA_INSTANCE_NAME:-cassandra}"
-readonly KUDO_CASSANDRA_INSTANCE_NAMESPACE="${KUDO_CASSANDRA_INSTANCE_NAMESPACE:-kudo-cassandra}"
+readonly SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+readonly PROJECT_DIRECTORY="$(readlink -f "${SCRIPT_DIRECTORY}/..")"
+
+# shellcheck source=../metadata.sh
+source "${PROJECT_DIRECTORY}/metadata.sh"
+
+kudo_cassandra_version=
+kudo_cassandra_instance_name=
+kudo_cassandra_instance_namespace=
+
+while [[ ${#} -gt 0 ]]; do
+  # TODO(mpereira): handle parameters passed in as "parameter=value";
+  parameter="${1}"
+
+  case "${parameter}" in
+    --version|-v)
+      kudo_cassandra_version="${2}"
+      shift
+      ;;
+    --instance|-i)
+      kudo_cassandra_instance_name="${2}"
+      shift
+      ;;
+    --namespace|-n)
+      kudo_cassandra_instance_namespace="${2}"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+
+  shift
+done
+
+kudo_cassandra_version="${kudo_cassandra_version:-0.1.0}"
+kudo_cassandra_instance_name="${kudo_cassandra_instance_name:-cassandra}"
+kudo_cassandra_instance_namespace="${kudo_cassandra_instance_namespace:-kudo-cassandra}"
 
 kubectl delete instance \
-        "${KUDO_CASSANDRA_INSTANCE_NAME}" \
-        -n "${KUDO_CASSANDRA_INSTANCE_NAMESPACE}"
+        "${kudo_cassandra_instance_name}" \
+        -n "${kudo_cassandra_instance_namespace}"
 
 kubectl delete operatorversion \
-        "${KUDO_CASSANDRA_OPERATOR_NAME}-${KUDO_CASSANDRA_VERSION}" \
-        -n "${KUDO_CASSANDRA_INSTANCE_NAMESPACE}"
+        "cassandra-${kudo_cassandra_version}" \
+        -n "${kudo_cassandra_instance_namespace}"
 
 kubectl delete operator \
-        "${KUDO_CASSANDRA_OPERATOR_NAME}" \
-        -n "${KUDO_CASSANDRA_INSTANCE_NAMESPACE}"
+        "cassandra" \
+        -n "${kudo_cassandra_instance_namespace}"
 
 declare -a PVCS
 mapfile -t PVCS < <(
   kubectl get pvc \
-          -n "${KUDO_CASSANDRA_INSTANCE_NAMESPACE}" \
+          -n "${kudo_cassandra_instance_namespace}" \
           -o 'jsonpath={.items[*].metadata.name}' \
     | tr ' ' '\n'
 )
 
 for pvc in "${PVCS[@]}"; do
-  kubectl delete "pvc/${pvc}" -n "${KUDO_CASSANDRA_INSTANCE_NAMESPACE}"
+  kubectl delete "pvc/${pvc}" -n "${kudo_cassandra_instance_namespace}"
 done
