bac6e490249a108e0d6a14c11c540e31200ef888
Murilo Pereira <murilo@murilopereira.com>
2019-11-01 21:05:34 +0100

    Update to KUDO 0.8.0-pre.2, abide to new APIs and specs.

--- metadata.sh
+++ metadata.sh
@@ -36,8 +36,8 @@ export VENDOR_DIRECTORY="${_project_directory}/shared/vendor"
 # https://github.com/docker-library/cassandra/blob/master/3.11/Dockerfile
 export CASSANDRA_VERSION="3.11.4"
 
-# https://github.com/kudobuilder/kudo/releases/tag/v0.7.4
-export KUDO_VERSION="0.7.5"
+# https://github.com/kudobuilder/kudo/releases/tag/v0.8.0-pre.2
+export KUDO_VERSION="0.8.0-pre.2"
 
 export KUBERNETES_VERSION="1.15.0"
 
