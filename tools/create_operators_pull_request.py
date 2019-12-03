2e3b0336c397d322b26770c090e150e7ec8d5944
Murilo Pereira <murilo@murilopereira.com>
2019-12-03 17:53:47 +0100

    Extract sequential function calls into semantic blocks.

--- tools/create_operators_pull_request.py
+++ tools/create_operators_pull_request.py
@@ -23,7 +23,7 @@ log = logging.getLogger(__name__)
 OPERATORS_REPOSITORY = "kudobuilder/operators"
 
 
-def main() -> int:
+def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Open a PR copying a KUDO Operator's files to the "
         + f"{OPERATORS_REPOSITORY} repository"
@@ -82,48 +82,37 @@ def main() -> int:
         help="Show debug output from all operations performed",
     )
 
-    args = parser.parse_args()
-    operator_repository = args.operator_repository
-    operator_name = args.operator_name
-    operator_git_tag = args.operator_git_tag
-    github_token = args.github_token
-    git_commit_message = args.git_commit_message or (
-        f"Release {operator_name} {operator_git_tag} (automated commit)."
+    return parser.parse_args()
+
+
+def prepare_git_repositories(
+    base_directory: str,
+    operators_repository_url: str,
+    operators_base_branch: str,
+    operators_branch: str,
+    operator_repository: str,
+    operator_git_tag: str,
+    git_user: str,
+    github_token: str,
+    debug: bool,
+) -> (int, str, str):
+    """TODO: docstring."""
+    rc, operators_directory, error_message = clone_repository(
+        operators_repository_url, operators_base_branch, base_directory, debug
     )
-    operators_base_branch = args.operators_base_branch
-    git_user = args.git_user
-    debug = args.debug
+    if rc != 0:
+        return rc, error_message, "", ""
 
-    operators_repository = OPERATORS_REPOSITORY
-    operators_repository_url = (
-        f"{git_user}@github.com:{operators_repository}.git"
-    )
-    operators_branch = (
-        f"{operator_name}_{operator_git_tag}_{random_short_string()}"
+    operator_repository_url = (
+        f"https://{git_user}:{github_token}@github.com"
+        + f"/{operator_repository}.git"
     )
 
-    with tempfile.mkdtemp("_kudo_operator_tools") as base_directory:
-        rc, operators_directory, error_message = clone_repository(
-            operators_repository_url,
-            operators_base_branch,
-            base_directory,
-            debug,
-        )
-        if rc != 0:
-            log.error(error_message)
-            return rc
-
-        operator_repository_url = (
-            f"https://{git_user}:{github_token}@github.com"
-            + f"/{operator_repository}.git"
-        )
-
-        rc, operator_directory, error_message = clone_repository(
-            operator_repository_url, operator_git_tag, base_directory, debug
-        )
-        if rc != 0:
-            log.error(error_message)
-            return rc
+    rc, operator_directory, error_message = clone_repository(
+        operator_repository_url, operator_git_tag, base_directory, debug
+    )
+    if rc != 0:
+        return rc, error_message, "", ""
 
     rc, stdout, stderr = run(
         f"git -C {operators_directory} checkout -b {operators_branch}",
@@ -134,6 +123,8 @@ def main() -> int:
             rc,
             f"Failed to create {OPERATORS_REPOSITORY} branch: "
             + f"{operators_branch}\nstdout:\n{stdout}\nstderr:\n{stderr}",
+            "",
+            "",
         )
 
     rc, stdout, stderr = run(
@@ -146,6 +137,8 @@ def main() -> int:
             f"Failed to get git user name from "
             + f"{operator_repository}@{operator_git_tag}"
             + f"\nstdout:\n{stdout}\nstderr:\n{stderr}",
+            "",
+            "",
         )
     git_user_name = stdout.strip()
 
@@ -159,6 +152,8 @@ def main() -> int:
             f"Failed to get git user email from "
             + f"{operator_repository}@{operator_git_tag}"
             + f"\nstdout:\n{stdout}\nstderr:\n{stderr}",
+            "",
+            "",
         )
     git_user_email = stdout.strip()
 
@@ -166,9 +161,20 @@ def main() -> int:
         operators_directory, git_user_name, git_user_email, debug
     )
     if rc != 0:
-        log.error(error_message)
-        return rc
+        return rc, error_message, "", ""
 
+
+def commit_copied_operator_files_and_push_branch(
+    operators_directory: str,
+    operators_repository: str,
+    operators_branch: str,
+    operator_directory: str,
+    operator_name: str,
+    operator_git_tag: str,
+    git_commit_message: bool,
+    debug: bool,
+):
+    """TODO: docstring."""
     versioned_operator_directory = (
         f"{operators_directory}/repository/{operator_name}/{operator_git_tag}"
     )
@@ -213,29 +219,87 @@ def main() -> int:
             + f"\nstdout:\n{stdout}\nstderr:\n{stderr}",
         )
 
-    success, http_response = create_pull_request(
-        operators_repository,
-        operators_base_branch,
-        operators_branch,
-        git_commit_message,
-        "",
-        github_token,
-        PROGRAM_NAME,
-        debug,
+
+def main() -> int:
+    args = parse_arguments()
+
+    operator_repository = args.operator_repository
+    operator_name = args.operator_name
+    operator_git_tag = args.operator_git_tag
+    github_token = args.github_token
+    git_commit_message = args.git_commit_message or (
+        f"Release {operator_name} {operator_git_tag} (automated commit)."
     )
-    if not success:
-        log.error(
-            f"Error creating pull request\n"
-            + f"response body: {http_response.read()}\n"
-            + f"response headers: {http_response.getheaders()}"
+    operators_base_branch = args.operators_base_branch
+    git_user = args.git_user
+    debug = args.debug
+
+    operators_repository = OPERATORS_REPOSITORY
+    operators_repository_url = (
+        f"{git_user}@github.com:{operators_repository}.git"
+    )
+    operators_branch = (
+        f"{operator_name}_{operator_git_tag}_{random_short_string()}"
+    )
+
+    with tempfile.mkdtemp("_kudo_dev") as base_directory:
+        (
+            rc,
+            error_message,
+            operators_directory,
+            operator_directory,
+        ) = prepare_git_repositories(
+            base_directory,
+            operators_repository_url,
+            operators_base_branch,
+            operators_branch,
+            operator_repository,
+            operator_git_tag,
+            git_user,
+            github_token,
+            debug,
         )
-        return 1
+        if rc != 0:
+            log.error(error_message)
+            return rc
 
-    pull_request_url = json.loads(http_response.read())["html_url"]
+        rc, error_message = commit_copied_operator_files_and_push_branch(
+            operators_directory,
+            operators_repository,
+            operators_branch,
+            operator_directory,
+            operator_name,
+            operator_git_tag,
+            git_commit_message,
+            debug,
+        )
+        if rc != 0:
+            log.error(error_message)
+            return rc
 
-    log.info(f"Successfully created PR: '{pull_request_url}'")
+        success, http_response = create_pull_request(
+            operators_repository,
+            operators_base_branch,
+            operators_branch,
+            git_commit_message,
+            "",
+            github_token,
+            PROGRAM_NAME,
+            debug,
+        )
+        if not success:
+            log.error(
+                f"Error creating pull request\n"
+                + f"response body: {http_response.read()}\n"
+                + f"response headers: {http_response.getheaders()}"
+            )
+            return 1
 
-    return 0
+        pull_request_url = json.loads(http_response.read())["html_url"]
+
+        log.info(f"Successfully created PR: '{pull_request_url}'")
+
+        return 0
 
 
 if __name__ == "__main__":
