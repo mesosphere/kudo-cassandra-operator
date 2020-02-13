2262eb93327949a93f2791cecf74a35037fcd2a1
Andreas Neumann <aneumann@mesosphere.com>
2020-02-13 16:45:09 +0100

    Add Params for external service (#46)
    * Add external service to allow access to Cassandra from outside the cluster
    
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- tests/suites/tls/tls_test.go
+++ tests/suites/tls/tls_test.go
@@ -25,7 +25,7 @@ var (
 	TestInstance      = fmt.Sprintf("%s-instance", OperatorName)
 	KubeConfigPath    = os.Getenv("KUBECONFIG")
 	OperatorDirectory = os.Getenv("OPERATOR_DIRECTORY")
-	// TODO(mpereira): read NodeCount from params.yaml.
+
 	NodeCount = 2
 	Client    = client.Client{}
 	Operator  = kudo.Operator{}
