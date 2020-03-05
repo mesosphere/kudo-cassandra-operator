cd1a05699623950a58aa91158a53c221c1559313
Andreas Neumann <aneumann@mesosphere.com>
2020-03-05 10:01:16 +0100

    Use named backup
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- images/medusa/medusa-frontend.py
+++ images/medusa/medusa-frontend.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 from medusa.medusacli import cli
 import sys
 import subprocess
 
 PORT_NUMBER = 8082
+
 
 # This class will handles any incoming request from
 # the browser
@@ -13,8 +15,10 @@ class myHandler(BaseHTTPRequestHandler):
 
     # Handler for the GET requests
     def do_GET(self):
-        if self.path == "/backup":
-            self.doBackup()
+        request_url = urlparse(self.path)
+
+        if request_url.path == "/backup":
+            self.doBackup(request_url)
             return
 
         self.send_response(200)
@@ -25,14 +29,29 @@ class myHandler(BaseHTTPRequestHandler):
         self.wfile.write(msg.encode("UTF-8"))
         return
 
-    def doBackup(self):
+    def doBackup(self, request_url):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         # Send the html message
-        self.wfile.write("Starting medusa\n".encode("UTF-8"))
 
-        res = subprocess.run( ["python3", "/usr/local/bin/medusa", "backup"], stdout=subprocess.PIPE, stderr=self.wfile)
+        cmd = ["python3", "/usr/local/bin/medusa"]
+
+        query_params = parse_qs(request_url.query)
+
+        # Global options first
+        if 'fqdn' in query_params:
+            cmd.extend(["--fqdn", query_params['fqdn'][0]])
+
+        cmd.append("backup")
+
+        # Backup args after the command
+        if 'name' in query_params:
+            cmd.extend(["--backup-name", query_params['name'][0]])
+
+        self.wfile.write(("Starting medusa: " + ' '.join([str(v) for v in cmd]) + "\n").encode("UTF-8"))
+
+        res = subprocess.run(args=cmd, stdout=subprocess.PIPE, stderr=self.wfile)
 
         end_message = "Done: " + str(res.returncode) + "\n"
 
