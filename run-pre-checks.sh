50e684bd1a2bd0b7d915e6a4c40a45fecd06533a
Andreas Neumann <aneumann@mesosphere.com>
2020-03-11 11:45:53 +0100

    Do only template check
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- run-pre-checks.sh
+++ run-pre-checks.sh
@@ -10,10 +10,6 @@ source "${project_directory}/metadata.sh"
 
 FAILED=0
 
-go get golang.org/x/tools/cmd/goimports
-goimports -d . | awk 'BEGIN{had_data=0}{print;had_data=1}END{exit had_data}'
-FAILED=${FAILED-$?}
-
 cd "${project_directory}"
 ./tools/compile_templates.sh --check-only
 FAILED=${FAILED-$?}
