7eb59552ef47fd9f2bd1b405a94e2e3c57b18724
Marcin Owsiany <mowsiany@D2iQ.com>
2020-03-23 16:26:56 +0100

    Use `$IMAGE_DISAMBIGUATION_SUFFIX` if set. (#66)
    - ignore disambiguation suffix when checking template expansion
    - when set, expand templates before testing and building images

--- run-pre-checks.sh
+++ run-pre-checks.sh
@@ -5,9 +5,5 @@ set -euxo pipefail
 readonly script_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 readonly project_directory="$(readlink -f "${script_directory}")"
 
-# shellcheck source=metadata.sh
-source "${project_directory}/metadata.sh"
-
-cd "${project_directory}"
-./tools/compile_templates.sh --check-only
-
+# We need to ignore the suffix for the purpose of checking templates.
+IMAGE_DISAMBIGUATION_SUFFIX="" "${project_directory}/tools/compile_templates.sh" --check-only
