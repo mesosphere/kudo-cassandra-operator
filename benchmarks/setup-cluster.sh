397c2ef53d2c758fd59bd5dca6d6c8e21ee0087b
Andreas Neumann <aneumann@mesosphere.com>
2020-04-02 11:28:38 +0200

    Updated mwt params
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- benchmarks/setup-cluster.sh
+++ benchmarks/setup-cluster.sh
@@ -15,8 +15,8 @@ EOF
 
 echo "Create Namespaces"
 
-NAMESPACES="cassandra-dry"
-#NAMESPACES="cassandra-4dc cassandra-2dc cassandra-1dc"
+#NAMESPACES="cassandra-dry"
+NAMESPACES="cass-4dc cass-3dc cass-2dc cass-2dc-par cass-1dc-big cass-1dc-small cass-1dc-small-par"
 
 for NS in ${NAMESPACES}; do
     kubectl create namespace "${NS}"
@@ -43,9 +43,13 @@ roleRef:
 EOF
 done
 
-dev-kudo install ../../operator --instance="cassandra-dry" --namespace="cassandra-dry" --parameter-file dry/params-dry.yaml
+#dev-kudo install ../../operator --instance="cassandra-dry" --namespace="cassandra-dry" --parameter-file dry/params-dry.yaml
 
-#dev-kudo install ./operator --instance="cassandra-4dc" --namespace="cassandra-4dc" --parameter-file benchmarks/mwt/4dc/params-4dc.yaml
-#dev-kudo install ./operator --instance="cassandra-2dc-a" --namespace="cassandra-2dc" --parameter-file benchmarks/mwt/2dc/params-2dc-a.yaml
-#dev-kudo install ./operator --instance="cassandra-2dc-b" --namespace="cassandra-2dc" --parameter-file benchmarks/mwt/2dc/params-2dc-b.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-4dc" --parameter-file benchmarks/mwt/4dc/params-4dc.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-3dc" --parameter-file benchmarks/mwt/3dc/params-3dc.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-2dc" --parameter-file benchmarks/mwt/2dc/params-2dc.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-2dc-par" --parameter-file benchmarks/mwt/2dc-par/params-2dc-par.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-1dc-big" --parameter-file benchmarks/mwt/1dc-big/params-1dc-big.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-1dc-small" --parameter-file benchmarks/mwt/1dc-small/params-1dc-small.yaml
+dev-kudo install ./operator --instance="cassandra" --namespace="cass-1dc-small-par" --parameter-file benchmarks/mwt/1dc-small-par/params-1dc-small-par.yaml
 
