c44d96c7303ac94c86e2dc056ffd324128dbf708
Andreas Neumann <aneumann@mesosphere.com>
2020-05-13 14:19:50 +0200

    Fixed call to bootstrap init Improved argument parsing in main.go
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- images/bootstrap/main.go
+++ images/bootstrap/main.go
@@ -17,25 +17,30 @@ func main() {
 	}
 	cassandraService := service.NewCassandraService(client)
 
-	if len(os.Args) > 1 {
-		command := os.Args[1]
-		switch command {
-		case "wait":
-			if err := cassandraService.Wait(); err != nil {
-				log.Errorf("bootstrap: timeout waiting for UN/UJ for Cassandra node: %v\n", err)
-				os.Exit(1)
-			}
-		case "init":
-			// bootstrap to fetch the replace IP
-			if err := cassandraService.SetReplaceIPWithRetry(); err != nil {
-				log.Errorf("bootstrap: could not run the cassandra bootstrap: %v\n", err)
-				os.Exit(1)
-			}
-			log.Infof("bootstrap: Finish Cassandra bootstrap init")
-		default:
-			log.Errorf("bootstrap: unrecognized command '%s' for cassandra bootstrap", command)
+	if len(os.Args) != 2 {
+		log.Errorf("bootstrap: Wrong number of arguments: %d, must be 2: %v", len(os.Args), os.Args)
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	switch command {
+	case "wait":
+		log.Infof("Start waiting for Cassandra to be up")
+		if err := cassandraService.Wait(); err != nil {
+			log.Errorf("bootstrap: timeout waiting for UN/UJ for Cassandra node: %v\n", err)
 			os.Exit(1)
 		}
+	case "init":
+		// bootstrap to fetch the replace IP
+		if err := cassandraService.SetReplaceIPWithRetry(); err != nil {
+			log.Errorf("bootstrap: could not run the cassandra bootstrap: %v\n", err)
+			os.Exit(1)
+		}
+		log.Infof("bootstrap: Finish Cassandra bootstrap init")
+	default:
+		log.Errorf("bootstrap: unrecognized command '%s' for cassandra bootstrap", command)
+		os.Exit(1)
 	}
+
 	os.Exit(0)
 }
