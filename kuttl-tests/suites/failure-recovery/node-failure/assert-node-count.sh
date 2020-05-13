fa95f9ffd7e7589fb6d5e544188c3284c015a4fb
Andreas Neumann <aneumann@mesosphere.com>
2020-05-13 14:37:14 +0200

    Fixed seed list
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- kuttl-tests/suites/failure-recovery/node-failure/assert-node-count.sh
+++ kuttl-tests/suites/failure-recovery/node-failure/assert-node-count.sh
@@ -5,10 +5,18 @@ EXPECTED_NODE_COUNT=$2
 # $3 is "--namespace"
 NAMESPACE=$4
 
-# Fetch the list of current nodes from cassandra and parse lines with UN/DN/... etc from the output
-ACTUAL_NODE_COUNT=`kubectl exec -n ${NAMESPACE} ${NODE_NAME} nodetool status | grep -E "^\w{2}\s+.*$" | wc -l`
+for i in {1..15}; do
+    # Fetch the list of current nodes from cassandra and parse lines with UN/DN/... etc from the output
+    ACTUAL_NODE_COUNT=`kubectl exec -n ${NAMESPACE} ${NODE_NAME} nodetool status | grep -E "^\w{2}\s+.*$" | wc -l`
 
-echo "Expected Node count: $EXPECTED_NODE_COUNT, Actual Node count: $ACTUAL_NODE_COUNT"
+    echo "Expected Node count: $EXPECTED_NODE_COUNT, Actual Node count: $ACTUAL_NODE_COUNT (Try $i)"
+
+    if [[ ${ACTUAL_NODE_COUNT} == ${EXPECTED_NODE_COUNT} ]]; then
+        echo "Found matching node count"
+        exit 0
+    fi
+    sleep 5
+done
 
 # Return corresponding exit code
-[[ ${ACTUAL_NODE_COUNT}  == ${EXPECTED_NODE_COUNT} ]]
+exit 1
