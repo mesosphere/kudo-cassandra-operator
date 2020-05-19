d8a1d4ed8aa0250026f0e33061f21aab98c5dcf5
Andreas Neumann <aneumann@mesosphere.com>
2020-05-19 10:55:45 +0200

    More debugging and a bugfix
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- kuttl-tests/suites/failure-recovery/node-failure/assert-node-down.sh
+++ kuttl-tests/suites/failure-recovery/node-failure/assert-node-down.sh
@@ -13,14 +13,18 @@ for i in {1..15}; do
 
     echo "Expected Down Node count: $EXPECTED_DOWN_NODE_COUNT, Actual Node count: $DOWN_NODE_COUNT (Try $i)"
 
-    DOWN_NODE_COUNT_ZERO=`kubectl exec -n ${NAMESPACE} cassandra-node-0 -- ${CMD} | grep -E "^DN\s+.*$" | wc -l`
-
-    echo "Same output from Node-0: $DOWN_NODE_COUNT_ZERO"
-
     if [[ ${EXPECTED_DOWN_NODE_COUNT} == ${DOWN_NODE_COUNT} ]]; then
         echo "Found matching down node count"
         exit 0
     fi
+
+    NODE_IPS=`kubectl exec -n ${NAMESPACE} cassandra-node-1 -- ${CMD} | sed -nE 's/^UN[[:space:]]+([0-9.]+).*/\1/'`
+    echo "Node IPS: $NODE_IPS"
+    for IP in ${NODE_IPS}; do
+        echo "Try Status on $IP"
+        kubectl exec -n ${NAMESPACE} ${NODE_NAME} -- nodetool --ssl --host ${IP} --port 7199 status
+    done
+
     sleep 10
 done
 
