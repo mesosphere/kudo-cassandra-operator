539648203137be87b2c112cf926eba97ff6d73cc
Andreas Neumann <aneumann@mesosphere.com>
2020-05-20 09:39:22 +0200

    Try cordon/delete pod/delete node
    Signed-off-by: Andreas Neumann <aneumann@mesosphere.com>

--- kuttl-tests/suites/failure-recovery/node-failure/delete-node-for-pod.sh
+++ kuttl-tests/suites/failure-recovery/node-failure/delete-node-for-pod.sh
@@ -6,10 +6,11 @@ NAMESPACE=$3
 
 NODE_NAME=`kubectl get pod ${POD_NAME} -n ${NAMESPACE} -o=custom-columns=NODE:.spec.nodeName --no-headers`
 
+echo "Cordon node $NODE_NAME"
+kubectl cordon ${NODE_NAME}
 
-echo "Killing docker node $NODE_NAME"
-# export DOCKER_API_VERSION=1.39
-# docker kill ${NODE_NAME}
+echo "Deleting pod {$POD_NAME}"
+kubectl delete pod ${POD_NAME}
 
 echo "Delete node $NODE_NAME"
 kubectl delete node ${NODE_NAME}
