9f4c67da6cf3e852785c0f0460f629456d5b87af
Jan Schlicht <jan@d2iq.com>
2020-08-24 14:18:00 +0200

    Fix paths

--- kuttl-tests/kind-test.sh
+++ kuttl-tests/kind-test.sh
@@ -26,21 +26,23 @@ mkdir -p bin/
 
 curl -Lo "bin/kubectl_${KUBECTL_VERSION}_${OS}" "https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/${OS}/${MACHINE}/kubectl"
 chmod +x "bin/kubectl_${KUBECTL_VERSION}_${OS}"
-ln -sf "bin/kubectl_${KUBECTL_VERSION}_${OS}" bin/kubectl
+ln -sf "./kubectl_${KUBECTL_VERSION}_${OS}" ./bin/kubectl
 
 curl -Lo "bin/kubectl-kuttl_${KUTTL_VERSION}_${OS}" "https://github.com/kudobuilder/kuttl/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_${OS}_${KUDO_MACHINE}"
 chmod +x "bin/kubectl-kuttl_${KUTTL_VERSION}_${OS}"
-ln -sf "bin/kubectl-kuttl_${KUTTL_VERSION}_${OS}" bin/kubectl-kuttl
+ln -sf "./kubectl-kuttl_${KUTTL_VERSION}_${OS}" ./bin/kubectl-kuttl
 
 curl -Lo "bin/kubectl-kudo_${KUBECTL_KUDO_VERSION}_${OS}" "https://github.com/kudobuilder/kudo/releases/download/v${KUBECTL_KUDO_VERSION}/kubectl-kudo_${KUBECTL_KUDO_VERSION}_${OS}_${KUDO_MACHINE}"
 chmod +x "bin/kubectl-kudo_${KUBECTL_KUDO_VERSION}_${OS}"
-ln -sf "bin/kubectl-kudo_${KUBECTL_KUDO_VERSION}_${OS}" bin/kubectl-kudo
+ln -sf "./kubectl-kudo_${KUBECTL_KUDO_VERSION}_${OS}" ./bin/kubectl-kudo
 
 curl -Lo "bin/kind_${KIND_VERSION}_${OS}" "https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/kind-${OS}-${MACHINE}"
 chmod +x "bin/kind_${KIND_VERSION}_${OS}"
-ln -sf "bin/kind_${KIND_VERSION}_${OS}" bin/kind
+ln -sf "./kind_${KIND_VERSION}_${OS}" ./bin/kind
 
 mkdir -p $ARTIFACTS
 go get github.com/jstemmer/go-junit-report
+
+PATH="$(pwd)/bin:${PATH}"
 kubectl kuttl test --config=./suites/kuttl-common.yaml --artifacts-dir=${ARTIFACTS} 2>&1 | tee /dev/fd/2 | go-junit-report -set-exit-code > kuttl-dist/common-junit.xml
 kubectl kuttl test --config=./suites/kuttl-failure-recovery.yaml --artifacts-dir=${ARTIFACTS} 2>&1 | tee /dev/fd/2 | go-junit-report -set-exit-code > kuttl-dist/failure-recovery-junit.xml
